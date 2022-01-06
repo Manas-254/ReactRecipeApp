048fa9c57812429fcd55b0b8d863dbe635bce472
Manas-254 <51674758+Manas-254@users.noreply.github.com>
2022-01-06 11:20:50 +0530

    Update RecipeApp.js

--- src/components/RecipeApp.js
+++ src/components/RecipeApp.js
@@ -13,10 +13,12 @@ function Recipe(props) {
                     return (
                         <div key={val+val.id} className="border border-secondary shadow p-3 mb-5 bg-body rounded">
                             <div  className="card" style={{ width: "18rem" }} onClick={()=>{sendRecipe(val.id,val.name,val.recipe,val.img)}}>
-                                <img src={val.img} className="card-img-top" alt="img" />
-                                <div  className="card-body">
+                            <div  className="card-body">
+                            <h5  className="card-title">{val.id}</h5>
                                     <h5  className="card-title">{val.name}</h5>
                                 </div>
+                                <img src={val.img} className="card-img-top" alt="img" />
+                                
                             </div><br />
                         </div>
                     )
